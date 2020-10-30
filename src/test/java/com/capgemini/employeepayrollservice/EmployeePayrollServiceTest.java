1acea2673155e4ad4ee00b0a0334961d45540a64
bab21 <2016061@iiitdmj.ac.in>
2020-10-30 20:21:19 +0530

    verified earlier use cases with the complete ER

--- src/test/java/com/capgemini/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/capgemini/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -11,45 +11,45 @@ import com.capgemini.employeepayrollservice.EmployeePayrollService.IOService;
 import com.capgemini.employeepayrollservice.model.EmployeePayrollData;
 
 public class EmployeePayrollServiceTest {
-	//UC2 Database..
-	@Test
-	public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
-		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
-		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-		System.out.println("data"+employeePayrollDataList);
-		assertEquals(3,employeePayrollDataList.size());
-	}
-	//UC3 Database...
-	@Test
-	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
-		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
-		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-		employeePayrollService.updateEmployeeSalary("Terisa",300000.00);
-		boolean result=employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
-		assertTrue(result);
-		
-	}
-	
-	//UC5....
-	@Test
-	public void givenDateRange_WhenEmployeeDataRetrieved_ShouldMatch() throws EmployeePayrollException {
-		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
-		LocalDate startDate=LocalDate.of(2018, 01, 01);
-		LocalDate endDate=LocalDate.now();
-		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.getEmployeeWithDateRange(startDate,endDate);
-		assertEquals(3,employeePayrollDataList.size());
-	}
-	//UC6...
+//	//UC2 Database..
 //	@Test
-//	public void givenEmployeeData_WhenSumSalaryWithGender_ShouldMatch() throws EmployeePayrollException {
+//	public void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() throws EmployeePayrollException {
 //		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
-//		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
-//		Map<String,Double> salarySumByGender=employeePayrollService.getEmployeeSalarySumGroupWithGender();
-//		assertTrue(salarySumByGender.get("F").equals(600000.0) && salarySumByGender.get("M").equals(100000.0));
+//		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+//		System.out.println("data"+employeePayrollDataList);
+//		assertEquals(3,employeePayrollDataList.size());
 //	}
+//	//UC3 Database...
+//	@Test
+//	public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() throws EmployeePayrollException {
+//		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+//		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+//		employeePayrollService.updateEmployeeSalary("Terisa",300000.00);
+//		boolean result=employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+//		assertTrue(result);
+//		
+//	}
+//	
+//	//UC5....
+//	@Test
+//	public void givenDateRange_WhenEmployeeDataRetrieved_ShouldMatch() throws EmployeePayrollException {
+//		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+//		LocalDate startDate=LocalDate.of(2018, 01, 01);
+//		LocalDate endDate=LocalDate.now();
+//		List<EmployeePayrollData> employeePayrollDataList=employeePayrollService.getEmployeeWithDateRange(startDate,endDate);
+//		assertEquals(3,employeePayrollDataList.size());
+//	}
+	//UC6...
+	@Test
+	public void givenEmployeeData_WhenSumSalaryWithGender_ShouldMatch() throws EmployeePayrollException {
+		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
+		Map<String,Double> salarySumByGender=employeePayrollService.getEmployeeSalarySumGroupWithGender();
+		assertTrue(salarySumByGender.get("F").equals(600000.0) && salarySumByGender.get("M").equals(100000.0));
+	}
 	
 	//UC7..
-//	@Test
+////	@Test
 //	public void givenNewEmployee_WhenAdded_ShouldSyncWithDB() throws EmployeePayrollException{
 //		EmployeePayrollService employeePayrollService=new EmployeePayrollService();
 //		employeePayrollService.readEmployeePayrollData(IOService.DB_IO);
